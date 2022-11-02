9ea2c602432b19bf85d8b681d3d1501ca42d78dd
RebekahEast <rebekah.jean.east@gmail.com>
2022-11-02 17:43:09 +0000

    commit

--- main.js
+++ main.js
@@ -1,6 +1,24 @@
-const readMoreBtn = document.querySelector('.read-more-btn');
-const text = document.querySelector('.text');
+const readMoreRefill = document.querySelector('.read-more-refill');
+const text1 = document.querySelector('.text1');
 
-readMoreBtn.addEventListener('click',(e)=>{
-    text.classList.toggle('show-more');
+readMoreRefill.addEventListener('click',()=>{
+    text1.classList.toggle('show-more');
+    if(readMoreRefill.innerText == 'Read More'){
+        readMoreRefill.innerText = 'Read Less';
+    }else {
+        readMoreRefill.innerText = 'Read More';
+    }
+})
+
+
+const readMoreFour = document.querySelector('.read-more-four');
+const text4 = document.querySelector('.text4');
+
+readMoreFour.addEventListener('click',()=>{
+    text4.classList.toggle('show-more');
+    if(readMoreFour.innerText == 'Read More'){
+        readMoreFour.innerText = 'Read Less';
+    }else {
+        readMoreFour.innerText = 'Read More';
+    }
 })
